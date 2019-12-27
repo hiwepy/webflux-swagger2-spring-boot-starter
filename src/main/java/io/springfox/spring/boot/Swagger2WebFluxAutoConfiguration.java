7c7b88a259a27e34c9b9a1bcfe39e1c5ec824952
wandl <hnxyhcwdl1003@163.com>
2019-12-27 16:52:05 +0800

    修改版权信息

--- src/main/java/io/springfox/spring/boot/Swagger2WebFluxAutoConfiguration.java
+++ src/main/java/io/springfox/spring/boot/Swagger2WebFluxAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, vindell (https://github.com/vindell).
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -68,7 +68,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 /**
  * 参考https://my.oschina.net/go4it/blog/3035218
  * 部分代码来来自：https://github.com/SpringForAll/spring-boot-starter-swagger
- * @author ： <a href="https://github.com/vindell">vindell</a>
+ * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
  */
 @Configuration
 @ConditionalOnProperty(prefix = Swagger2WebFluxProperties.PREFIX, value = "enabled", havingValue = "true")
