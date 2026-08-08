e6faf93c3436e5725ef155584dc024297d9dab1e
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:55 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/test/java/io/springfox/spring/boot/Swagger2UiWebFluxConfigurerTest.java
+++ src/test/java/io/springfox/spring/boot/Swagger2UiWebFluxConfigurerTest.java
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {{ @link Swagger2UiWebFluxConfigurer }}.
  *
- * @author wandl
+ * @author [@Loong Wan](https://github.com/loong10k)
  * @since 1.0.0
  */
 @DisplayName("Swagger2UiWebFluxConfigurer Tests")
